d3b76fa58b66411279546a2cdccd115db7043632
Азрет Елеев <azreteleev@MacBook-Air-Azret.local>
2023-02-22 23:53:33 +0300

    start simple section

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
-    
+    <simple-section />
   </div>
 </template>
 
 <script>
+import SimpleSection from './components/SimpleSection.vue';
+
 export default {
   name: 'App',
+  components: {
+    SimpleSection,
+  },
 };
 </script>
 
---
+++ src/components/SimpleSection.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="simple-section">
+    <h2 class="simple-section__title">Add your title</h2>
+    <p class="simple-section__content">Add your content</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SimpleSection',
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
