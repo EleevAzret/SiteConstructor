bdace3adfbe0711f9f990db4aecb0d4ec4c0b1ee
Азрет Елеев <azreteleev@MacBook-Air-Azret.local>
2023-02-24 17:43:49 +0300

    add colorPicker

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="color-picker">
+    <b-dropdown variant="outline">
+      <template #button-content>
+        <b-icon icon="droplet-fill" />
+      </template>
+      <b-dropdown-group header="Choose color" class="small">
+        <b-dropdown-item-button
+          v-for="(value, color) in colors"
+          :key="color"
+          :data-type="color"
+          @click="selectColor">
+            <b-icon :icon="value.icon" aria-hidden="true" />
+            {{ value.name }}
+            <b-icon icon="square-fill" :variant="color" class="color-icon" />
+        </b-dropdown-item-button>
+      </b-dropdown-group>
+    </b-dropdown>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ColorPicker',
+  data: () => ({
+    colors: {
+      'primary': {
+        name: 'Dark Blue',
+        icon: 'blank',
+      },
+      'success': {
+        name: 'Green',
+        icon: 'blank',
+      },
+      'warning': {
+        name: 'Yellow',
+        icon: 'blank',
+      },
+      'danger': {
+        name: 'red',
+        icon: 'blank',
+      },
+      'info': {
+        name: 'Light Blue',
+        icon: 'blank',
+      },
+      'light': {
+        name: 'Light',
+        icon: 'check',
+      },
+      'dark': {
+        name: 'Dark',
+        icon: 'blank',
+      },
+    },
+  }),
+  methods: {
+    selectColor(e) {
+      let color = e.currentTarget.dataset.type;
+      this.refreshIcons();
+      this.colors[color].icon = 'check';
+      this.$emit('changeColor', color);
+    },
+    refreshIcons() {
+      for (let color in this.colors) {
+        this.colors[color].icon = 'blank';
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+  .color-icon {
+    float: right;
+  }
+</style>
